5e23ae0844806503fd9e39b7571b6786ea5fdf39
Edmartt <47486245+Edmartt@users.noreply.github.com>
2024-09-30 13:48:02 -0500

    feat: adds feature for getting commerce campaigns results

--- internal/adapters/campaign.go
+++ internal/adapters/campaign.go
@@ -29,8 +29,21 @@ func (c *CampaignRepository) SaveCampaign(campaign domain.Campaign) (*domain.Cam
 	return &campaign, nil
 }
 
-func (c *CampaignRepository) ReadCommerceCampaign(id string) (*domain.Campaign, error) {
+func (c *CampaignRepository) ReadCommerceCampaign(id string) (*domain.CommerceCampaign, error) {
+	dbConnection, err := c.dbConnection.GetConnection()
 
+	if err != nil {
+		return nil, fmt.Errorf("error connecting to db: %v", err)
+	}
+
+	var result domain.CommerceCampaign
+
+	err = dbConnection.Get(result, "SELECT c.id AS campaign_id, c.campaign_name, c.campaign_multiplier, c.campaign_percent_bonus, c.start_date, c.end_date, co.id AS commerce_id, co.name AS commerce_name, co.points_x_buy, co.value_x_point, co.created_at FROM campaign c JOIN commerce co ON c.commerce_id = co.id WHERE co.id = ?", id)
+
+	if err != nil {
+		return nil, fmt.Errorf("error fetching campaign data: %v", err)
+	}
+	return &result, nil
 }
 
 func (c *CampaignRepository) ReadBranchCampaign(id string) (*domain.Branch, error) {
