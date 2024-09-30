99de0f7b9d3e0df38c131cdf42fcd9bc9a027ca1
Edmartt <47486245+Edmartt@users.noreply.github.com>
2024-09-30 10:15:52 -0500

    feat: adds branch service layer

---
+++ internal/core/services/branches_service.go
@@ -0,0 +1,23 @@
+package services
+
+import (
+	"github.com/Edmartt/loyalty-system/internal/core/domain"
+	"github.com/Edmartt/loyalty-system/internal/core/ports"
+)
+
+type BranchService struct {
+	campaignRepository ports.Campaign
+	branchRepository   ports.Branch
+}
+
+func (b *BranchService) CreateBranch(branch domain.Branch) (*domain.Branch, error) {
+	return b.branchRepository.CreateBranch(branch)
+}
+
+func (b *BranchService) GetBranchCampaign(id string) (*domain.Branch, error) {
+	return b.campaignRepository.ReadBranchCampaign(id)
+}
+
+func (b *BranchService) CreateBranchCampaig(campaign domain.Campaign) (*domain.Campaign, error) {
+	return b.campaignRepository.SaveCampaign(campaign)
+}
