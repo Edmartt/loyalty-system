66f81cc00c94f5af530f9ad209258f5df40d3aaf
Edmartt <47486245+Edmartt@users.noreply.github.com>
2024-09-30 17:13:26 -0500

    feat: adds new handler for http customers

--- internal/adapters/http/gin/customer_handlers.go
+++ internal/adapters/http/gin/customer_handlers.go
@@ -4,6 +4,8 @@ import (
 	"net/http"
 
 	localHttp "github.com/Edmartt/loyalty-system/internal/adapters/http"
+	"github.com/Edmartt/loyalty-system/internal/adapters/http/dto"
+	"github.com/Edmartt/loyalty-system/internal/core/domain"
 	"github.com/Edmartt/loyalty-system/internal/core/services"
 	"github.com/gin-gonic/gin"
 )
@@ -33,4 +35,47 @@ func (c CustomerHandlers) GetUser(context *gin.Context) {
 	}
 
 	context.JSON(http.StatusOK, customer)
+}
+
+func (c CustomerHandlers) PostUser(context *gin.Context) {
+
+	customerDTO := dto.CustomerDTO{}
+	response := localHttp.HttpResponse{}
+
+	err := context.BindJSON(&customerDTO)
+
+	if err != nil {
+		response.Response = "invalid request: " + err.Error()
+		context.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	if customerDTO.UserDNI == "" || customerDTO.Name == "" || customerDTO.LastName == "" || customerDTO.Phone == "" || customerDTO.Email == "" {
+
+		response.Response = "missing required data"
+		context.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	customer := domain.Customer{
+		UserDNI:  customerDTO.UserDNI,
+		Name:     customerDTO.Name,
+		LastName: customerDTO.LastName,
+		Phone:    customerDTO.Phone,
+		Email:    customerDTO.Email,
+	}
+
+	_, err = c.CustomerService.CreateUser(customer)
+
+	if err != nil {
+		response.Response = "error creating user: " + err.Error()
+		context.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	withObjectResponse := localHttp.HttpCustomerCreated{
+		Message:  "customer created successfully",
+		Response: customerDTO,
+	}
+	context.JSON(http.StatusOK, withObjectResponse)
 }
