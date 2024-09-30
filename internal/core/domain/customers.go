0af52dcbbd2fb009d551d74d0edf406b7e948e30
Edmartt <47486245+Edmartt@users.noreply.github.com>
2024-09-30 16:30:04 -0500

    chore: renames with capital for exporting

--- internal/core/domain/customers.go
+++ internal/core/domain/customers.go
@@ -3,9 +3,9 @@ package domain
 // Customer is an entity handling commerce customers
 type Customer struct {
 	ID                string `db:"id"`
-	userDNI           string `db:"user_dni"`
+	UserDNI           string `db:"user_dni"`
 	Name              string `db:"last_name"`
-	lastName          string `db:"last_name"`
+	LastName          string `db:"last_name"`
 	Phone             string `db:"phone"`
 	Email             string `db:"email"`
 	PointsCollected   int64  `db:"points_collected"`
