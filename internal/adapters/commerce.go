c2ffa6493d69b7608761adf5130abc86b63c9e32
Edmartt <47486245+Edmartt@users.noreply.github.com>
2024-09-30 11:47:03 -0500

    feat: adds implementations for saving and reading value per points and points earned per commerce

--- internal/adapters/commerce.go
+++ internal/adapters/commerce.go
@@ -32,3 +32,76 @@ func (c *CommerceRepository) SaveCommerce(domain.Commerce) (*domain.Commerce, er
 
 	return &domain.Commerce{}, nil
 }
+
+func (c *CommerceRepository) ReadPointsXBuy(id string) (*domain.Commerce, error) {
+	dbConnection, err := c.dbConnection.GetConnection()
+
+	if err != nil {
+		return nil, fmt.Errorf("error connecting to db: %v", err)
+	}
+
+	var commerce domain.Commerce
+
+	err = dbConnection.Get(&commerce, "SELECT points_x_buy FROM commerce WHERE id=?", id)
+
+	if err != nil {
+		return nil, fmt.Errorf("error fetching commerce points", err)
+	}
+
+	return &commerce, nil
+}
+
+func (c *CommerceRepository) SavePointsXBuy(id string, points int32) (string, error) {
+	dbConnection, err := c.dbConnection.GetConnection()
+
+	if err != nil {
+		return "", fmt.Errorf("error connecting to db: %v", err)
+	}
+
+	_, err = dbConnection.Exec("UPDATE commerce SET points_x_buy = $1 WHERE id = $2", points, id)
+
+	if err != nil {
+		return "", fmt.Errorf("error updating commerce points: %v", err)
+	}
+
+	message := fmt.Sprintf("current commerce with ID %s given points updated to %d", id, points)
+
+	return message, nil
+}
+
+func (c *CommerceRepository) ReadValueXPoint(id string) (*domain.Commerce, error) {
+	dbConnection, err := c.dbConnection.GetConnection()
+
+	if err != nil {
+		return nil, fmt.Errorf("error connecting to db: %v", err)
+	}
+
+	var commerce domain.Commerce
+
+	err = dbConnection.Get(&commerce, "SELECT value_x_point FROM commerce WHERE id=?", id)
+
+	if err != nil {
+		return nil, fmt.Errorf("error fetching commerce value per point", err)
+	}
+
+	return &commerce, nil
+}
+
+func (c *CommerceRepository) SaveValueXPoint(id string, value int64) (string, error) {
+
+	dbConnection, err := c.dbConnection.GetConnection()
+
+	if err != nil {
+		return "", fmt.Errorf("error connecting to db: %v", err)
+	}
+
+	_, err = dbConnection.Exec("UPDATE commerce SET value_x_point = $1 WHERE id = $2", value, id)
+
+	if err != nil {
+		return "", fmt.Errorf("error updating commerce points: %v", err)
+	}
+
+	message := fmt.Sprintf("current commerce with ID %s given value updated to %d", id, value)
+
+	return message, nil
+}
