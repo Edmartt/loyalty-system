1c97aa272fa37bba2375879e3d3ad81af247a8bb
Edmartt <47486245+Edmartt@users.noreply.github.com>
2024-09-30 11:43:05 -0500

    chore: adds comment for clarification of purpose

--- internal/core/domain/commerce.go
+++ internal/core/domain/commerce.go
@@ -3,6 +3,6 @@ package domain
 type Commerce struct {
 	ID          string `db:"id"`
 	Name        string `db:"name"`
-	PointsXbuy  int64  `db:"points_x_buy"`
-	ValueXPoint int64  `db:"value_x_point"`
+	PointsXbuy  int64  `db:"points_x_buy"`  // points rewarded for purchases in stores
+	ValueXPoint int64  `db:"value_x_point"` // how much money must be spent to obtain the points offered by the commerce
 }
