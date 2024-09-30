11d0a60438738d69588959d91d0128122a788e57
Edmartt <47486245+Edmartt@users.noreply.github.com>
2024-09-30 10:15:28 -0500

    feat: adds commerce service

---
+++ internal/core/services/commerce_service.go
@@ -0,0 +1,23 @@
+package services
+
+import (
+	"github.com/Edmartt/loyalty-system/internal/core/domain"
+	"github.com/Edmartt/loyalty-system/internal/core/ports"
+)
+
+type CommerceService struct {
+	commerceRepository ports.Commerce
+	campaignRepository ports.Campaign
+}
+
+func (c *CommerceService) CreateCommerce(commerce domain.Commerce) (*domain.Commerce, error) {
+	return c.commerceRepository.CreateCommerce(commerce)
+}
+
+func (c *CommerceService) GetCommerceCampaign(campaignID string) (*domain.Campaign, error) {
+	return c.campaignRepository.ReadCommerceCampaign(campaignID)
+}
+
+func (c *CommerceService) SetCommerceCampaign(campaign domain.Campaign) (*domain.Campaign, error) {
+	return c.campaignRepository.SaveCampaign(campaign)
+}
