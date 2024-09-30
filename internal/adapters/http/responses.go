2c4db5f71b2e85b69a946c62d6404846ff69e09c
Edmartt <47486245+Edmartt@users.noreply.github.com>
2024-09-30 16:43:53 -0500

    update: update http response for customer created with the object created and message

--- internal/adapters/http/responses.go
+++ internal/adapters/http/responses.go
@@ -1,5 +1,12 @@
 package http
 
+import "github.com/Edmartt/loyalty-system/internal/core/domain"
+
 type HttpResponse struct {
 	Response string `json:"response"`
+}
+
+type HttpCustomerCreated struct {
+	Message  string          `json:"message"`
+	Response domain.Customer `json:"customer"`
 }
