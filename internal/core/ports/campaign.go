486b9e34940a43443737249502c78c570f3d4af2
Edmartt <47486245+Edmartt@users.noreply.github.com>
2024-09-30 13:56:59 -0500

    update: rename interfaces for improving readability

--- internal/core/ports/campaign.go
+++ internal/core/ports/campaign.go
@@ -4,6 +4,6 @@ import "github.com/Edmartt/loyalty-system/internal/core/domain"
 
 type Campaign interface {
 	SaveCampaign(campaign domain.Campaign) (*domain.Campaign, error)
-	ReadCommerceCampaign(id string) (*domain.Campaign, error)
-	ReadBranchCampaign(id string) (*domain.Branch, error)
+	ReadCommerceCampaign(id string) (*domain.BranchCampaign, error)
+	ReadBranchCampaign(id string) (*domain.BranchCampaign, error)
 }
