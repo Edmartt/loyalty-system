5fc52a12cf07ec01c83008519691073f7fd71609
Edmartt <47486245+Edmartt@users.noreply.github.com>
2024-09-30 11:44:41 -0500

    update: names are updated to refer to the data layer and new ports are added

--- internal/core/ports/commerce.go
+++ internal/core/ports/commerce.go
@@ -3,5 +3,9 @@ package ports
 import "github.com/Edmartt/loyalty-system/internal/core/domain"
 
 type Commerce interface {
-	CreateCommerce(domain.Commerce) (*domain.Commerce, error)
+	SaveCommerce(domain.Commerce) (*domain.Commerce, error)
+	ReadPointsXBuy(string) (*domain.Commerce, error)
+	SavePointsXBuy(string, int32) (string, error)
+	ReadValueXPoint(id string) (*domain.Commerce, error)
+	SaveValueXPoint(string, int64) (string, error)
 }
