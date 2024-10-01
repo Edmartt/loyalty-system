d0d63ec0484fab9e37408c63e6a2a3fd913c818f
Edmartt <47486245+Edmartt@users.noreply.github.com>
2024-10-01 02:08:10 -0500

    update: campaign multiplier and campaign bonus now are float types

--- internal/core/domain/campaign_commerce.go
+++ internal/core/domain/campaign_commerce.go
@@ -3,10 +3,11 @@ package domain
 import "time"
 
 type CommerceCampaign struct {
-	CampaignID           string    `json:"campaign_id" db:"campaign_id"`
-	CampaignName         string    `json:"campaign_name" db:"campaign_name"`
-	CampaignMultiplier   int       `json:"campaign_multiplier" db:"campaign_multiplier"`
-	CampaignPercentBonus int       `json:"campaign_percent_bonus" db:"campaign_percent_bonus"`
+	CampaignID   string `json:"campaign_id" db:"campaign_id"`
+	CampaignName string `json:"campaign_name" db:"campaign_name"`
+	float64
+	CampaignMultiplier   float64   `json:"campaign_multiplier" db:"campaign_multiplier"`
+	CampaignPercentBonus float64   `json:"campaign_percent_bonus" db:"campaign_percent_bonus"`
 	StartDate            time.Time `json:"start_date" db:"start_date"`
 	EndDate              time.Time `json:"end_date" db:"end_date"`
 	CommerceID           string    `json:"commerce_id" db:"commerce_id"`
