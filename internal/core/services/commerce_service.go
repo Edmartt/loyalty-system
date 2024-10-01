354a6a882550538b5750d6b776f56208815e631e
Edmartt <47486245+Edmartt@users.noreply.github.com>
2024-09-30 23:31:08 -0500

    update: adds constructor for injecting dependencies and renames params in main struct

--- internal/core/services/commerce_service.go
+++ internal/core/services/commerce_service.go
@@ -6,34 +6,41 @@ import (
 )
 
 type CommerceService struct {
-	commerceRepository ports.Commerce
-	campaignRepository ports.Campaign
+	CommerceRepository ports.Commerce
+	CampaignRepository ports.Campaign
+}
+
+func NewCommerceService(commerceRepository ports.Commerce, campaignRepository ports.Campaign) *CommerceService {
+	return &CommerceService{
+		CommerceRepository: commerceRepository,
+		CampaignRepository: campaignRepository,
+	}
 }
 
 func (c *CommerceService) CreateCommerce(commerce domain.Commerce) (*domain.Commerce, error) {
-	return c.commerceRepository.SaveCommerce(commerce)
+	return c.CommerceRepository.SaveCommerce(commerce)
 }
 
 func (c *CommerceService) GetCommerceCampaign(campaignID string) (*domain.CommerceCampaign, error) {
-	return c.campaignRepository.ReadCommerceCampaign(campaignID)
+	return c.CampaignRepository.ReadCommerceCampaign(campaignID)
 }
 
 func (c *CommerceService) SetCommerceCampaign(campaign domain.Campaign) (*domain.Campaign, error) {
-	return c.campaignRepository.SaveCampaign(campaign)
+	return c.CampaignRepository.SaveCampaign(campaign)
 }
 
 func (c *CommerceService) GetCommercePointsXBuy(id string) (*domain.Commerce, error) {
-	return c.commerceRepository.ReadPointsXBuy(id)
+	return c.CommerceRepository.ReadPointsXBuy(id)
 }
 
 func (c *CommerceService) SetCommercePointsXBuy(id string, commercePoint int32) (string, error) {
-	return c.commerceRepository.SavePointsXBuy(id, commercePoint)
+	return c.CommerceRepository.SavePointsXBuy(id, commercePoint)
 }
 
 func (c *CommerceService) SetCommerceValueXPoint(id string, money int64) (string, error) {
-	return c.commerceRepository.SaveValueXPoint(id, money)
+	return c.CommerceRepository.SaveValueXPoint(id, money)
 }
 
 func (c *CommerceService) GetCommerceValueXPoint(id string) (*domain.Commerce, error) {
-	return c.commerceRepository.ReadValueXPoint(id)
+	return c.CommerceRepository.ReadValueXPoint(id)
 }
