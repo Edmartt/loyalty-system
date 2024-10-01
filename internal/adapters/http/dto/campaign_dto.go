b36df9d9d52c7f96119438247d4f583dba8e5a72
Edmartt <47486245+Edmartt@users.noreply.github.com>
2024-10-01 12:44:38 -0500

    update: updates time.Time fields to strings for parsing with yyyy-mm-dd format

--- internal/adapters/http/dto/campaign_dto.go
+++ internal/adapters/http/dto/campaign_dto.go
@@ -1,15 +1,13 @@
 package dto
 
-import "time"
-
 type CampaignDTO struct {
-	ID                   string    `json:"id"`
-	CommerceID           string    `json:"commerce_id"`
-	BranchID             string    `json:"branch_id"`
-	CampaignName         string    `json:"campaign_name"`
-	CampaignType         string    `json:"campaign_type"`
-	CampaignMultiplier   int32     `json:"campaign_multiplier"`
-	CampaignPercentBonus int16     `json:"campaign_percent_bonus"`
-	StartDate            time.Time `json:"start_date"`
-	EndDate              time.Time `json:"end_date"`
+	ID                   string `json:"id"`
+	CommerceID           string `json:"commerce_id"`
+	BranchID             string `json:"branch_id"`
+	CampaignName         string `json:"campaign_name"`
+	CampaignType         string `json:"campaign_type"`
+	CampaignMultiplier   int32  `json:"campaign_multiplier"`
+	CampaignPercentBonus int16  `json:"campaign_percent_bonus"`
+	StartDate            string `json:"start_date"`
+	EndDate              string `json:"end_date"`
 }
