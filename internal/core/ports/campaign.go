2737cf04b69516eb0d7842cd48d76dbd380c2c9a
Edmartt <47486245+Edmartt@users.noreply.github.com>
2024-10-01 02:09:22 -0500

    update: ReadCommerceCampaign now returns a slice of CommerceCampaign

--- internal/core/ports/campaign.go
+++ internal/core/ports/campaign.go
@@ -4,6 +4,6 @@ import "github.com/Edmartt/loyalty-system/internal/core/domain"
 
 type Campaign interface {
 	SaveCampaign(campaign domain.Campaign) (*domain.Campaign, error)
-	ReadCommerceCampaign(id string) (*domain.CommerceCampaign, error)
+	ReadCommerceCampaign(id string) ([]domain.CommerceCampaign, error)
 	ReadBranchCampaign(id string) (*domain.BranchCampaign, error)
 }
