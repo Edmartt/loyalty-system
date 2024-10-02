d999851e8dedca89b8b3ebc5b1cbed3f620b6791
Edmartt <47486245+Edmartt@users.noreply.github.com>
2024-10-02 00:37:29 -0500

    update: removes methods related to commerce and branch

--- internal/adapters/campaign.go
+++ internal/adapters/campaign.go
@@ -2,7 +2,6 @@ package adapters
 
 import (
 	"fmt"
-	"log"
 
 	"github.com/Edmartt/loyalty-system/internal/core/domain"
 	ports "github.com/Edmartt/loyalty-system/internal/core/ports/database"
@@ -32,42 +31,4 @@ func (c *CampaignRepository) SaveCampaign(campaign domain.Campaign) (*domain.Cam
 	}
 
 	return &campaign, nil
-}
-
-func (c *CampaignRepository) ReadCommerceCampaign(id string) ([]domain.CommerceCampaign, error) {
-
-	dbConnection, err := c.dbConnection.GetConnection()
-
-	if err != nil {
-		return nil, fmt.Errorf("error connecting to db: %v", err)
-	}
-
-	var result []domain.CommerceCampaign
-
-	err = dbConnection.Select(&result, "SELECT c.id AS campaign_id, c.campaign_name, c.campaign_multiplier, c.campaign_percent_bonus, c.start_date, c.end_date, co.id AS commerce_id, co.name AS commerce_name, co.points_x_buy, co.value_x_point FROM campaign c JOIN commerce co ON c.commerce_id = co.id WHERE co.id = $1", id)
-
-	if err != nil {
-		log.Println(err.Error())
-		return nil, fmt.Errorf("error fetching campaign data: %v", err)
-	}
-
-	return result, nil
-}
-
-func (c *CampaignRepository) ReadBranchCampaign(id string) ([]domain.BranchCampaign, error) {
-
-	dbConnection, err := c.dbConnection.GetConnection()
-
-	if err != nil {
-		return nil, fmt.Errorf("error connecting to db: %v", err)
-	}
-
-	var result []domain.BranchCampaign
-
-	err = dbConnection.Select(&result, "SELECT c.id AS campaign_id, c.campaign_name, c.campaign_multiplier, c.campaign_percent_bonus, c.start_date, c.end_date, b.id AS branch_id, b.address, co.id AS commerce_id, co.name AS commerce_name, co.points_x_buy, co.value_x_point FROM campaign c JOIN branch b ON c.branch_id = b.id JOIN commerce co ON c.commerce_id = co.id WHERE b.id = $1 ", id)
-
-	if err != nil {
-		return nil, fmt.Errorf("error fetching campaign data: %v", err)
-	}
-	return result, nil
 }
