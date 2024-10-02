8c2e8cd7f318b2744bf806f37563a1b455fb411e
Edmartt <47486245+Edmartt@users.noreply.github.com>
2024-10-02 00:32:51 -0500

    update: adds 2 more methods related to branches

--- internal/core/ports/branch.go
+++ internal/core/ports/branch.go
@@ -1,7 +1,12 @@
 package ports
 
-import "github.com/Edmartt/loyalty-system/internal/core/domain"
+import (
+	"github.com/Edmartt/loyalty-system/internal/adapters/http/dto"
+	"github.com/Edmartt/loyalty-system/internal/core/domain"
+)
 
 type Branch interface {
 	CreateBranch(domain.Branch) (*domain.Branch, error)
+	CreateBranchCampaign(domain.Campaign) (*domain.Campaign, error)
+	ReadBranchCampaign(string) ([]dto.BranchCampaign, error)
 }
