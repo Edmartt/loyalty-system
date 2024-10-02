afc521b6b6776404c397bf2e7f2c96767a7e730a
Edmartt <47486245+Edmartt@users.noreply.github.com>
2024-10-02 00:36:28 -0500

    feat: adds read and create branch campaign implementations

--- internal/adapters/branch.go
+++ internal/adapters/branch.go
@@ -3,6 +3,7 @@ package adapters
 import (
 	"fmt"
 
+	"github.com/Edmartt/loyalty-system/internal/adapters/http/dto"
 	"github.com/Edmartt/loyalty-system/internal/core/domain"
 	ports "github.com/Edmartt/loyalty-system/internal/core/ports/database"
 )
@@ -31,4 +32,39 @@ func (b *BranchRepository) CreateBranch(branch domain.Branch) (*domain.Branch, e
 	}
 
 	return &branch, nil
+}
+
+func (b *BranchRepository) ReadBranchCampaign(id string) ([]dto.BranchCampaign, error) {
+
+	dbConnection, err := b.dbConnection.GetConnection()
+
+	if err != nil {
+		return nil, fmt.Errorf("error connecting to db: %v", err)
+	}
+
+	var result []dto.BranchCampaign
+
+	err = dbConnection.Select(&result, "SELECT c.id AS campaign_id, c.campaign_name, c.campaign_multiplier, c.campaign_percent_bonus, c.start_date, c.end_date, b.id AS branch_id, b.address, co.id AS commerce_id, co.name AS commerce_name, co.points_x_buy, co.value_x_point FROM campaign c JOIN branch b ON c.branch_id = b.id JOIN commerce co ON c.commerce_id = co.id WHERE b.id = $1 ", id)
+
+	if err != nil {
+		return nil, fmt.Errorf("error fetching campaign data: %v", err)
+	}
+	return result, nil
+}
+
+func (b *BranchRepository) CreateBranchCampaign(campaign domain.Campaign) (*domain.Campaign, error) {
+
+	dbConnection, err := b.dbConnection.GetConnection()
+
+	if err != nil {
+		return nil, fmt.Errorf("error connecting to DB: %v", err)
+	}
+
+	_, err = dbConnection.NamedExec("INSERT INTO campaign(commerce_id, branch_id, campaign_name, campaign_type, campaign_multiplier, campaign_percent_bonus, start_date, end_date) VALUES(:commerce_id, :branch_id, :campaign_name, :campaign_type, :campaign_multiplier,:campaign_percent_bonus, :start_date, :end_date)", &campaign)
+
+	if err != nil {
+		return nil, fmt.Errorf("error saving campaign data: %v", err)
+	}
+
+	return &campaign, nil
 }
