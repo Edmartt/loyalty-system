b442a17fa16de7e6df164a42d0e140e394cbc9c2
Edmartt <47486245+Edmartt@users.noreply.github.com>
2024-10-02 00:34:02 -0500

    update: adds 2 more related methods to commerce campaigns

--- internal/core/ports/commerce.go
+++ internal/core/ports/commerce.go
@@ -4,6 +4,8 @@ import "github.com/Edmartt/loyalty-system/internal/core/domain"
 
 type Commerce interface {
 	SaveCommerce(domain.Commerce) (*domain.Commerce, error)
+	ReadCommerceCampaign(string) ([]domain.Campaign, error)
+	CreateCommerceCampaign(domain.Campaign) (*domain.Campaign, error)
 	ReadPointsXBuy(string) (*domain.Commerce, error)
 	SavePointsXBuy(string, int32) (string, error)
 	ReadValueXPoint(id string) (*domain.Commerce, error)
