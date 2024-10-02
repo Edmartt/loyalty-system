72de8d95c137fa398f018a10b6de4f988210d5f3
Edmartt <47486245+Edmartt@users.noreply.github.com>
2024-10-02 00:34:52 -0500

    update: removes campaign repository and just work with branch repository

--- internal/core/services/branches_service.go
+++ internal/core/services/branches_service.go
@@ -1,19 +1,18 @@
 package services
 
 import (
+	"github.com/Edmartt/loyalty-system/internal/adapters/http/dto"
 	"github.com/Edmartt/loyalty-system/internal/core/domain"
 	"github.com/Edmartt/loyalty-system/internal/core/ports"
 )
 
 type BranchService struct {
-	campaignRepository ports.Campaign
-	branchRepository   ports.Branch
+	branchRepository ports.Branch
 }
 
-func NewBranchService(branch ports.Branch, repository ports.Campaign) *BranchService {
+func NewBranchService(branch ports.Branch) *BranchService {
 	return &BranchService{
-		campaignRepository: repository,
-		branchRepository:   branch,
+		branchRepository: branch,
 	}
 }
 
@@ -21,10 +20,10 @@ func (b *BranchService) CreateBranch(branch domain.Branch) (*domain.Branch, erro
 	return b.branchRepository.CreateBranch(branch)
 }
 
-func (b *BranchService) GetBranchCampaign(id string) ([]domain.BranchCampaign, error) {
-	return b.campaignRepository.ReadBranchCampaign(id)
+func (b *BranchService) GetBranchCampaign(id string) ([]dto.BranchCampaign, error) {
+	return b.branchRepository.ReadBranchCampaign(id)
 }
 
-func (b *BranchService) CreateBranchCampaig(campaign domain.Campaign) (*domain.Campaign, error) {
-	return b.campaignRepository.SaveCampaign(campaign)
+func (b *BranchService) CreateBranchCampaign(campaign domain.Campaign) (*domain.Campaign, error) {
+	return b.branchRepository.CreateBranchCampaign(campaign)
 }
