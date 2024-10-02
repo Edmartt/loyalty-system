3b84a0dd74ad64cf972d7df065e0c65d35abe6ca
Edmartt <47486245+Edmartt@users.noreply.github.com>
2024-10-02 00:38:23 -0500

    feat: adds read and create methods implementations for commerce campaigns

--- internal/adapters/commerce.go
+++ internal/adapters/commerce.go
@@ -2,6 +2,7 @@ package adapters
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/Edmartt/loyalty-system/internal/core/domain"
 	ports "github.com/Edmartt/loyalty-system/internal/core/ports/database"
@@ -15,6 +16,26 @@ func NewCommerceRepository(dbConnection ports.IConnection) *CommerceRepository {
 	return &CommerceRepository{
 		dbConnection: dbConnection,
 	}
+}
+
+func (c *CommerceRepository) ReadCommerceCampaign(id string) ([]domain.Campaign, error) {
+
+	dbConnection, err := c.dbConnection.GetConnection()
+
+	if err != nil {
+		return nil, fmt.Errorf("error connecting to db: %v", err)
+	}
+
+	var result []domain.Campaign
+
+	err = dbConnection.Select(&result, "SELECT c.id AS campaign_id, c.campaign_name, c.campaign_multiplier, c.campaign_percent_bonus, c.start_date, c.end_date, co.id AS commerce_id, co.name AS commerce_name, co.points_x_buy, co.value_x_point FROM campaign c JOIN commerce co ON c.commerce_id = co.id WHERE co.id = $1", id)
+
+	if err != nil {
+		log.Println(err.Error())
+		return nil, fmt.Errorf("error fetching campaign data: %v", err)
+	}
+
+	return result, nil
 }
 
 func (c *CommerceRepository) SaveCommerce(commerce domain.Commerce) (*domain.Commerce, error) {
@@ -31,6 +52,23 @@ func (c *CommerceRepository) SaveCommerce(commerce domain.Commerce) (*domain.Com
 	}
 
 	return &commerce, nil
+}
+
+func (c *CommerceRepository) CreateCommerceCampaign(campaign domain.Campaign) (*domain.Campaign, error) {
+
+	dbConnection, err := c.dbConnection.GetConnection()
+
+	if err != nil {
+		return nil, fmt.Errorf("error connecting to DB: %v", err)
+	}
+
+	_, err = dbConnection.NamedExec("INSERT INTO campaign(commerce_id, branch_id, campaign_name, campaign_type, campaign_multiplier, campaign_percent_bonus, start_date, end_date) VALUES(:commerce_id, :branch_id, :campaign_name, :campaign_type, :campaign_multiplier,:campaign_percent_bonus, :start_date, :end_date)", &campaign)
+
+	if err != nil {
+		return nil, fmt.Errorf("error saving campaign data: %v", err)
+	}
+
+	return &campaign, nil
 }
 
 func (c *CommerceRepository) ReadPointsXBuy(id string) (*domain.Commerce, error) {
