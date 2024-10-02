45133277688110dbb56c6142e03fc7820094637a
Edmartt <47486245+Edmartt@users.noreply.github.com>
2024-10-02 00:33:25 -0500

    update: removes related branch method

--- internal/core/ports/campaign.go
+++ internal/core/ports/campaign.go
@@ -5,5 +5,4 @@ import "github.com/Edmartt/loyalty-system/internal/core/domain"
 type Campaign interface {
 	SaveCampaign(campaign domain.Campaign) (*domain.Campaign, error)
 	ReadCommerceCampaign(id string) ([]domain.CommerceCampaign, error)
-	ReadBranchCampaign(id string) ([]domain.BranchCampaign, error)
 }
